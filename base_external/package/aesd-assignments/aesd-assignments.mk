dfeb34c6c08e6c914919a0b4c7cde70bcdf42ed1
simo-2021 <at.simo@pm.me>
2025-12-29 15:47:42 +0100

    Fix aesd-assignments.mk (tabs + minimal install)

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,35 +1,44 @@
 ##############################################################
 #
 # AESD-ASSIGNMENTS
-#
+# modification: 27DEC2025, by Arnaud SIMO
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESD_ASSIGNMENTS_VERSION = adb4b0c9146ef04fc2d85004d7fc28a620bd10b1
+
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-simo-2021.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-simo-2021.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
+ # Buildroot n’installe que ce que tu déclares explicitement dans AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+define AESD_ASSIGNMENTS_BUILD_CMDS 
     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
     # Créer les répertoires de configuration
-    $(INSTALL) -d -m 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+    $(INSTALL) -d -m 0755  $(TARGET_DIR)/etc/finder-app/conf/
+    
     # Copier les fichiers de configuration
     $(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+
     # Installer les scripts et exécutables
     $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
+    
+    #NEW 
+    #$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/finder-test.sh $(TARGET_DIR)/usr/bin/
+
+    
     # Copier les fichiers de test si nécessaire
-    $(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin
+    #$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
