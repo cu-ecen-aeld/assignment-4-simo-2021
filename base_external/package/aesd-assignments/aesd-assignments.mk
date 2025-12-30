1da5c3c414fd50380d434651e4fe64347f88cb77
simo-2021 <at.simo@pm.me>
2025-12-30 13:48:13 +0100

    aesd-assignments.mk 13h48

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,7 +4,7 @@
 # modification: 27DEC2025, by Arnaud SIMO
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = cb8b5229f7e6c5617ffca42fca1951cf80c1043d
+AESD_ASSIGNMENTS_VERSION = 2a2175b5907adff9b352dc09618a5e40ad20c6e6 
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-simo-2021.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
@@ -16,7 +16,7 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	# Configuration
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf
-	$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0644 $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 
 	# Binaire writer
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
